3645cbb0cf7f50d7ae45bb44f03a789628be5b7b
Aleksey Belchenkov <u608110@gmail.com>
2021-01-17 15:24:33 +0300

    Relations Between Models

--- api/app/Models/ParentOfStudent.php
+++ api/app/Models/ParentOfStudent.php
@@ -4,15 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ParentOfStudent extends Model
 {
     use HasFactory;
 
     protected $table = 'parent_of_students';
-
     protected $fillable = [
         'fio',
         'phone'
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function students(): belongsToMany
+    {
+        return $this->belongsToMany(
+            Student::class,
+            'student_parent',
+            'parent_id',
+            'student_id'
+        );
+    }
 }
--- api/app/Models/Student.php
+++ api/app/Models/Student.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
     use HasFactory;
 
     protected $table = 'students';
+    public $timestamps = true;
 
     protected $fillable = [
         'fio',
@@ -17,4 +19,17 @@ class Student extends Model
         'email',
         'address'
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function parents(): belongsToMany
+    {
+        return $this->belongsToMany(
+            ParentOfStudent::class,
+            'student_parent',
+            'student_id',
+            'parent_id'
+        );
+    }
 }
